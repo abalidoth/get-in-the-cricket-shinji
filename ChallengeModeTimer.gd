f143683469d7a1898cbba7e291d4fc77708f2a41
clermorgaine <clermorgaine@gmail.com>
2024-03-17 12:16:18 -0500

    challenge mode fix

--- ChallengeModeTimer.gd
+++ ChallengeModeTimer.gd
@@ -1,5 +1,5 @@
 extends Timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_paused(GameSettings.challenge_mode)
+	set_paused(!GameSettings.challenge_mode)
 	set_wait_time ( 1 / GameSettings.challenge_speed)
