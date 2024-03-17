1db2f16abb2874aa54cb50c29a49ea99feacb14e
Gavin Daphne King <gdking01@gmail.com>
2024-03-17 00:28:41 -0700

    adding seed

--- GameSettings.gd
+++ GameSettings.gd
@@ -3,14 +3,18 @@ extends Node
 @export var challenge_mode := false
 @export var challenge_speed := 1
 @export var worm_ticks := 4
+@export var seed := 1
 #and then the keyboard bindings
 
+signal updated()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func update():
+	updated.emit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
