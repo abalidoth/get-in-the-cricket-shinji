de4ba455360df99e16aaa072de0dc29e2beaea63
clermorgaine <clermorgaine@gmail.com>
2024-03-17 03:36:55 -0500

    keybind_changer.tscn

--- csharpscripts/KeybindChanger.cs
+++ csharpscripts/KeybindChanger.cs
@@ -3,69 +3,69 @@ using System.Linq;
 
 public partial class KeybindChanger : Button
 {
-    [Export]
-    string InputActionName;
-    string CurrentKeybind;
-    Node GameSettings;
+	[Export]
+	string InputActionName;
+	string CurrentKeybind;
+	Node GameSettings;
 
-    bool Active = false;
+	bool Active = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        CurrentKeybind = (InputMap.ActionGetEvents(InputActionName).First() as InputEventKey).AsTextKeyLabel(); ;
-        Text = CurrentKeybind;
-        GameSettings = GetNode("/root/GameSettings");
+		CurrentKeybind = (InputMap.ActionGetEvents(InputActionName).First() as InputEventKey).AsTextKeyLabel(); ;
+		Text = CurrentKeybind;
+		GameSettings = GetNode("/root/GameSettings");
 
-    }
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        if (!Active) return;
-        if (@event is InputEventKey inputEventKey && @event.IsPressed())
-        {
-            
-            if (inputEventKey.Keycode == Key.Escape)
-            {
-                Active = false;
-                Text = CurrentKeybind;
-                return;
-            }
-            CurrentKeybind = inputEventKey.AsTextKeyLabel();
-            InputMap.ActionEraseEvents(InputActionName);
-            InputMap.ActionAddEvent(InputActionName, inputEventKey);
-            GameSettings.EmitSignal("keybind_changed", CurrentKeybind);  
-            Text = CurrentKeybind;
-        }
-    }
+	public override void _Input(InputEvent @event)
+	{
+		if (!Active) return;
+		if (@event is InputEventKey inputEventKey && @event.IsPressed())
+		{
+			
+			if (inputEventKey.Keycode == Key.Escape)
+			{
+				Active = false;
+				Text = CurrentKeybind;
+				return;
+			}
+			CurrentKeybind = inputEventKey.AsTextKeyLabel();
+			InputMap.ActionEraseEvents(InputActionName);
+			InputMap.ActionAddEvent(InputActionName, inputEventKey);
+			GameSettings.EmitSignal("keybind_changed", CurrentKeybind);  
+			Text = CurrentKeybind;
+		}
+	}
 
-    public override void _Pressed()
-    {
-        if (Active)
-        {
-            Active = false;
-            Text = CurrentKeybind;
-        }
-        else
-        {
-            Active = true;
-            Text = "???";
-        }
-    }
+	public override void _Pressed()
+	{
+		if (Active)
+		{
+			Active = false;
+			Text = CurrentKeybind;
+		}
+		else
+		{
+			Active = true;
+			Text = "???";
+		}
+	}
 
-    public void OnBindingChanged(string NewBinding)
-    {
-        if (Active)
-        {
-            Active = false;
-            return;
-        }
-        if (NewBinding == CurrentKeybind)
-        {
-            InputMap.ActionEraseEvents(InputActionName);
-            CurrentKeybind = "";
-            Text = "";
-        }
-    }
+	public void OnBindingChanged(string NewBinding)
+	{
+		if (Active)
+		{
+			Active = false;
+			return;
+		}
+		if (NewBinding == CurrentKeybind)
+		{
+			InputMap.ActionEraseEvents(InputActionName);
+			CurrentKeybind = "";
+			Text = "";
+		}
+	}
 
 
 
